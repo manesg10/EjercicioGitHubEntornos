7fed5132436035493d7b8ff6c90f0b451237efd8
Borja029 <161723679+Borja029@users.noreply.github.com>
2025-05-20 10:42:03 +0200

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,13 @@
+dias = []
+dias.append("Lunes")
+dias.append("Martes")
+dias.append("Miercoles")
+print(dias)
+dias.remove("Martes")
+print(dias)
+# dias.remove("ddd")
+elem = dias.pop()
+print(elem)
+print(dias)
+
+
